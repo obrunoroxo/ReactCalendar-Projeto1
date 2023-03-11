98453437c3af3e5d0d477a631618b9ad468c05eb
Bruno Rossi <bruno.rossi22@outlook.com>
2023-03-11 00:32:29 -0300

    Adicionei modal de retorno de dados após consulta por CPF.

--- src/components/modalComponents/consultSchedule.js
+++ src/components/modalComponents/consultSchedule.js
@@ -1,18 +1,38 @@
 import * as React from "react";
-import { useContext } from "react";
 import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
+import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
+import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
+import dayjs from 'dayjs';
+
 import { ModalContext } from "../context/ModalContext";
+import { getAllAPIDatas } from "../reqsAPI/reqAllDatasApi";
+
 
 export default function ConsultSchedule() {
-  const { open1, setOpen1 } = useContext(ModalContext);
-  const { clientcpf, setClientCPF } = React.useState("");
-  // const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const { open1, setOpen1 } = React.useContext(ModalContext);
+  const [clientcpf, setClientCPF] = React.useState("");
+  const [openModalDatas, setOpenModalDatas] = React.useState(false);
+  const [userData, setUserData] = React.useState({ data: "", idhora: "", tipoHora: "" });
+
+  async function handleClick(cpf) {
+    const response = await getAllAPIDatas();
+    const schedule = response['schedule'];
+    const datas = schedule.find(data => data['cpf'] === cpf);
+
+    return (
+      setUserData({
+        ...userData,
+        idhora: datas['idHora'],
+        data: datas['data'],
+        tipoHora: datas['tipoHora']
+      })
+    )
+  }
 
   return (
     <>
@@ -59,7 +79,7 @@ export default function ConsultSchedule() {
               label="Required"
               variant="filled"
               id="filled-required"
-              placeholder="Username"
+              placeholder="Your personal CPF"
               onChange={(event) => {
                 setClientCPF(event.target.value);
               }}
@@ -74,7 +94,13 @@ export default function ConsultSchedule() {
             spacing={2}
             direction="row"
           >
-            <Button variant="contained" onClick={() => {}}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                handleClick(clientcpf);
+                setOpenModalDatas(true);
+                setOpen1(false);
+              }}>
               OK
             </Button>
             <Button
@@ -86,6 +112,38 @@ export default function ConsultSchedule() {
               Cancel
             </Button>
           </Stack>
+        </Box>
+      </Modal>
+      <Modal
+        keepMounted
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        open={openModalDatas}
+      >
+        <Box
+          sx={{
+            m: 1,
+            p: 4,
+            top: "50%",
+            width: 400,
+            left: "50%",
+            boxShadow: 24,
+            position: "absolute",
+            bgcolor: "background.paper",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Stack direction="rows" spacing={1}>
+            <Chip
+              // centralizar o elemento
+              label={`${dayjs(userData.data).format('DD/MM/YYYY')} | ${userData.tipoHora} | ${userData.idhora}`}
+              variant="outlined"
+            />
+          </Stack>
+          <Button
+            onClick={() => setOpenModalDatas(false)}
+          > Cancel
+          </Button>
         </Box>
       </Modal>
     </>
